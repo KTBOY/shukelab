960620c658dbed0d6cbd64dd19fd3281b9b0bd06
Zlc <Zlc@123.com>
2022-09-06 18:01:14 +0800

    fix:完善首页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,44 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+	<view class="content">
+		<view class="head">
+			<view class="cover">
+				<image class="logo" src="https://p2.music.126.net/rNQH185Dp2lcG2yII_Aiwg==/18159534045164927.jpg"></image>
+			</view>
+			<view class="name"> Welcome to shuke lab</view>
+		</view>
+	</view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from 'vue';
+const title = ref('Hello');
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+<style lang="scss" scoped> 
+.head{
+	padding: 20rpx 30rpx;
+}
+.cover {
+  position: relative;
+  width: 120rpx;
+  height: 120rpx;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 3rpx solid #f1f8fd;
+  box-shadow:20rpx 20rpx 20rpx rgba(0,0,0,0.07),
+    -20rpx -20rpx 20rpx rgba(255,255,255,0.7),
+    6rpx 6rpx 6rpx rgba(0,0,0,0.09),
+    -6rpx -6rpx 6rpx rgba(255,255,255,0.9);
+	.logo{
+		width: 100%;
+		height: 100%;
+	}
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
+.name{
+	color: #aeafb0;
 }
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+
 </style>
